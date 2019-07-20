690cc96c191cc43cfb1015e2020883a68daf1daf
bugzweiser <bugzweiser@gmail.com>
2019-07-20 12:44:42 -0700

    forgot to commit for awhile...

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 
     {
-        "name": "Ahmed",
+        "name": "bob",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
             5,
